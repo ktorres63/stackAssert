16cecc0cda9865c59eff367d05746a6a505b7724
karlo <ktorresaro@unsa.edu.pe>
2022-11-01 17:39:00 -0500

    corrigiendo identacion y ordenando

--- my_project/AssertStack/arrayStack.cc
+++ my_project/AssertStack/arrayStack.cc
@@ -1,86 +1,94 @@
-#include <assert.h>
-#include <iostream>
 #include "arrayStack.h"
 #include "stack.h"
+#include <assert.h>
+#include <iostream>
 
-template<typename T>
-void ArrayStack<T>::push(T e){
-	//assert(data<sp); //si es false se cumple assert
-					 //data siempre debe estar en pos 0
+template <typename T>
+void ArrayStack<T>::push(T e) {
+  // assert(data<sp); //si es false se cumple assert
+  // data siempre debe estar en pos 0
 
-	int numElem = sp -data;
-	if(size == numElem){
-		//TODO
-		//assert(size>numElem)  // si no hubiese resize
-		resize();
-	}
-	*sp = e;
-	sp++;
+  int numElem = sp - data;
+  if (size == numElem) {
+	// TODO
+	// assert(size>numElem)  // si no hubiese resize
+	resize();
+  }
+  *sp = e;
+  sp++;
 
 }
-template<typename T>
-bool ArrayStack<T>::empty(){
-	return sp==data;
+
+template <typename T> void ArrayStack<T>::pop() {
+  // assert(sp>0);
+  if (empty())
+	return;
+  sp--;
 }
-template<typename T>
-void ArrayStack<T>::resize(){
-	//assert(size > 0);
-	T *newData = new T[size+FACTOR]; //TODO
-	for(int i = 0; i < size; i++){
-		newData[i] = data[i];
-	}
-	sp = newData + size;
-	size+= FACTOR;
-	delete[] data;
-	data = newData;
-}
-template<typename T>
-T ArrayStack<T>::top(){
-	assert(!empty());
-	return *(sp-1);
+template <typename T> T ArrayStack<T>::top() {
+  assert(!empty());
+  return *(sp - 1);
 }
 
-template<typename T>
-void ArrayStack<T>::pop(){
-	//assert(sp>0);
-	if(empty())
-		return;
-	sp--;
+template <typename T>
+bool ArrayStack<T>::empty() {
+  return sp == data;
 }
 
-template<typename T>
-T ArrayStack<T>::pop1(){
-	assert(!empty());
-	sp--;
-	return *(sp);
-}
-template<typename T>
-int ArrayStack<T>::tamOcup(){
-	int tam =(sp-1);
-	return tam;
-}
 
-template<typename T>
-void ArrayStack<T>::printStack(){
-	assert(data<sp);
-	T *pointer;
-	pointer = data;
-	for(int i =0; i < (sp-1);i++){
-		std::cout << *(pointer+i) << ", ";
-	}
-/* Otra forma
-	for (auto val: data){
-		std::cout << val << ", ";
-	}
-*/
-	std::cout << "\n";
+
+template <typename T>
+void ArrayStack<T>::resize() {
+  // assert(size > 0);
+  T *newData = new T[size + FACTOR]; // newData[]
+  for (int i = 0; i < size; i++) {
+	newData[i] = data[i];
+  }
+  sp = newData + size;
+  size += FACTOR;
+  delete[] data;
+  data = newData;
+}
+template <typename T> T ArrayStack<T>::pop1() {
+  assert(!empty());
+  sp--;
+  return *(sp);
 }
 
-int main(){
-	Stack<int> *ptr = new ArrayStack<int>(11);
-	ptr->push(1);
-	ptr->push(22);
+template <typename T> int ArrayStack<T>::tamOcup() {
+  // int tam =(sp-1);
+  //	return tam;
+}
 
-	std::cout << "hola\n";
-	return 0;
+template <typename T> void ArrayStack<T>::printStack() {
+  //	assert(data<sp);
+  /*
+		  T *pointer;
+		  pointer = data;
+		  int a = (sp-1)+0;
+		  for(int i =0; i < (sp-1);i++){
+				  std::cout << *(pointer+i) << ", ";
+		  }
+  */
+
+  std::cout << "\n";
+}
+
+int main() {
+  Stack<int> *ptr = new ArrayStack<int>(11);
+  ptr->push(1);
+  ptr->push(22);
+
+  ArrayStack<int> *a = new ArrayStack<int>(22);
+
+  a->push(11);
+  a->push(199);
+  a->push(1);
+
+  int valu = a->pop1();
+  std::cout << valu << "\n";
+
+  std::cout << "is empty? " << a->empty() << "\n";
+
+  return 0;
 }
--- my_project/AssertStack/arrayStack.h
+++ my_project/AssertStack/arrayStack.h
@@ -4,27 +4,25 @@
 #define FACTOR 10
 #include <iostream>
 
-template<typename T>
-class ArrayStack: public Stack<T>{
-	private:
-		T *data; // T data[]
-		int size; //memory size
-		T* sp; // stack pointer
-		void resize();
-	public:
-		ArrayStack(int s): size{s}, data{new T[size]}, sp{data}{}; //sp{&data[0]
-		~ArrayStack(){ delete [] data;}
-		void push(T);
-		void pop();
-		T top();
-		bool empty();
-		T pop1();
-		int tamOcup(); //tamaño ocupado del arreglo
-//		void print(std::ostream &os);
-		void printStack();
-		
+template <typename T>
+class ArrayStack : public Stack<T> {
+private:
+  T *data;  // T data[]
+  int size; // memory size
+  T *sp;    // stack pointer
+  void resize();
+
+public:
+  ArrayStack(int s) : size{s}, data{new T[size]}, sp{data} {}; // sp{&data[0]
+  ~ArrayStack() { delete[] data; }
+  void push(T);
+  void pop();
+  T top();
+  bool empty();
+  //---------------//
+  T pop1();
+  int tamOcup(); // tamaño ocupado del arreglo
+  //void print(std::ostream &os);
+  void printStack();
 };
 #endif
-
-
-
