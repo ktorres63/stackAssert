8befdafdbcc572c0b8da67c60aec207972d5ecc7
karlo <ktorresaro@unsa.edu.pe>
2022-11-01 10:55:48 -0500

    avance en append de List

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include "arrayStack.cc"
+#include "arrayStack.h"
+#include "stack.h"
+#include<iostream>
+int main(){
+    Stack<ArrayStack> a = new ArrayStack<int> (11);
+
+
+//	stk.push(3);
+
+	std::cout << "hola" << "\n";
+
+}
--- my_project/AssertList/list.cc
+++ my_project/AssertList/list.cc
@@ -5,28 +5,41 @@ List::List(){
   last = nullptr;
 }
 List* List::append(int e){
-  if(isEmpty()){
+  Node *n =new Node;
+  n->data = e;
+  n->next = nullptr;
+
+  if(head == nullptr){
+    head = new Node;
+    head->data = e;
+    head->next = nullptr;
+    return this;
+  }
+  else{
+    Node *n = new Node;
+    n->data = e;
+    n->next = nullptr;
+
+
     last = new Node;
     last->data = e;
     last->next = nullptr;
     return this;
   }
-  Node* n = new Node;
+  Node *n = new Node;
   n->data = e;
   n->next = last->next;
   last->next = n;
   last = n;
   return this;
 }
-int List::headA(){
-  return last->next->data;
-}
-int List::size(){
+int List::headA() { return last->next->data; }
+int List::size() {
   int c = 0;
-  Node* iter = last;
-  while(iter){
+  Node *iter = last;
+  while (iter) {
     iter = iter->next;
-    if(iter == last)
+    if (iter == last)
       iter = nullptr;
     c++;
   }
