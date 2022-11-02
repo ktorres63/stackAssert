dcb36445c81a4ee55d38191be4c16d33540e5841
karlo <ktorresaro@unsa.edu.pe>
2022-11-02 02:23:31 -0500

    añadiendo mas casos de prueba en arrayStack

--- my_project/hello_test.cc
+++ my_project/hello_test.cc
@@ -7,9 +7,10 @@
 // Demonstrate some basic assertions.
 TEST(Stack, Assertions) {
   Stack<int> *p = new ArrayStack<int>(11);
+  EXPECT_TRUE(p->empty());
   p->push(1);
   p->push(12);
-  EXPECT_EQ(p->empty(), false);
+
 }
 
 TEST(ArrayStack, Assertions) {
@@ -17,7 +18,12 @@ TEST(ArrayStack, Assertions) {
   p->push(8);
   p->push(3);
   p->push(9);
+  //p->pop();
   EXPECT_FALSE(p->empty());
+  EXPECT_EQ(p->pop1(),9);
+  EXPECT_NE(p->top(),9);
+
+
 }
 TEST(List, Assertions) {
   List *a = new List();
@@ -28,6 +34,7 @@ TEST(List, Assertions) {
   EXPECT_EQ(tam, a->size());
   EXPECT_FALSE(a->isEmpty());
   EXPECT_EQ(a->get(0), 0);
+
   a->append(0,99);
   EXPECT_EQ(99,a->get(0));
 
